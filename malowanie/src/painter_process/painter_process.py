e5733509591598fb1c8a744a51bc245fde679efe
kasztelmacko <152562189+kasztelmacko@users.noreply.github.com>
2025-04-20 08:26:41 +0000

    painitng enviroment initializator

--- malowanie/src/painter_process/painter_process.py
+++ malowanie/src/painter_process/painter_process.py
@@ -1,4 +1,13 @@
-from malowanie.src.process_classes.process_base import ProcessBase
+from malowanie.src.process_classes.process_base import ProcessBase, InitializePainterProcess
+from malowanie.src.process_classes.image_base import ImageBase
 
 class CreatePaintingFromBlankCanvasProcess(ProcessBase):
-    pass
+    def generate(self, target_image_name: str):
+        image = ImageBase(image_name = target_image_name)
+        process_initializator = InitializePainterProcess(
+            target_image = image,
+            scaled_max_width = 1024
+            )
+        scaled_image, blank_cavas = process_initializator.generate()
+        image.show_image(scaled_image)
+        image.show_image(blank_cavas)
