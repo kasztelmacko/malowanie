6ae78c81c7517acf6a0fd1e062e7d2d457417eac
kasztelmacko <152562189+kasztelmacko@users.noreply.github.com>
2025-04-20 08:26:14 +0000

    minor runscripts adjustments

--- malowanie/runscripts/painter_process.py
+++ malowanie/runscripts/painter_process.py
@@ -1,10 +1,15 @@
 #%%
 from malowanie.src.painter_process.painter_process import CreatePaintingFromBlankCanvasProcess
+import traceback
 
 #%%
 if __name__ == "__main__":
     try:
-        painter_process = CreatePaintingFromBlankCanvasProcess("TEST.jpg")
-        painter_process.generate()
-    except:
-        pass
+        painter_process = CreatePaintingFromBlankCanvasProcess()
+        painter_process.generate(
+            target_image_name="TEST.jpg"
+        )
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        traceback.print_exc()
+# %%
